7afd7791fb320aec0f53885a973e1d399481f071
Wiebe Kaai <wiebekaai@gmail.com>
2023-09-07 20:05:12 +0200

    asd

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,66 @@
-import traverseDOM from '@/lib/traverse-dom';
-
-const registeringOrRegisteredElements = new Set<string>();
-
-const importTsOrJs = async (path: string) =>
-  (
-    await Promise.all(
-      ['ts', 'js'].map(async (e) => {
-        try {
-          (await import(`${path}.${e}`)).default;
-        } catch (e) {
-          return null;
-        }
-      }),
-    )
-  ).filter(Boolean)[0];
-
-const registerElements = async (node: HTMLElement) =>
-  traverseDOM(node, async (currentNode) => {
-    const identifier = currentNode.tagName.toLowerCase();
-    const isCustomElement = identifier.includes('-');
-
-    if (isCustomElement) {
-      if (!registeringOrRegisteredElements.has(identifier)) {
-        registeringOrRegisteredElements.add(identifier);
-
-        console.log(`Registering ${identifier}`, currentNode);
-
-        const element = await importTsOrJs(`./elements/${identifier}`);
-
-        if (element) {
-          customElements.define(identifier, element);
-        } else {
-          console.warn(`No controller found for ${identifier}`, currentNode);
-        }
+const observeAddedElementNodes = (node: HTMLElement, callback: (node: HTMLElement) => unknown) => {
+  const observer = new window.MutationObserver((mutations) => {
+    for (let i = 0; i < mutations.length; i++) {
+      const { addedNodes } = mutations[i];
+      for (let j = 0; j < addedNodes.length; j++) {
+        const node = addedNodes[j];
+        if (node.nodeType === Node.ELEMENT_NODE) callback(node as HTMLElement);
       }
     }
   });
 
+  observer.observe(node, {
+    childList: true,
+    subtree: true,
+  });
+};
+
+const registerElements = async (node: HTMLElement) =>
+  [node, ...Array.from(node.querySelectorAll('*'))]
+    .filter((e) => e.tagName.includes('-'))
+    .forEach((node) => {
+      (async () => {
+        const identifier = node.tagName.toLowerCase();
+
+        // await request idle, with a timeout fallback for browsers that don't support it
+        await (window.requestIdleCallback ? window.requestIdleCallback : (cb: () => unknown) => setTimeout(cb, 100))(
+          () => {},
+        );
+
+        /**
+         * Support TypeScript and JavaScript
+         */
+        const element = await (
+          await Promise.all([
+            (async () => {
+              try {
+                return (await import(`./elements/${identifier}.ts`)).default;
+              } catch {
+                return null;
+              }
+            })(),
+            (async () => {
+              try {
+                return (await import(`./elements/${identifier}.js`)).default;
+              } catch {
+                return null;
+              }
+            })(),
+          ])
+        ).filter(Boolean)[0];
+
+        if (element) {
+          if (!customElements.get(identifier)) customElements.define(identifier, element);
+        } else {
+          console.error(
+            `Element <${identifier}> not found! Please create it.
+
+1. Create \`src/elements/${identifier}.ts\` or \`src/elements/${identifier}.js\`
+2. Add \`export default class extends HTMLElement {}\``,
+          );
+        }
+      })();
+    });
+
 registerElements(document.body);
+observeAddedElementNodes(document.body, registerElements);
