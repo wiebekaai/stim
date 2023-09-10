a7cdcdf520ebbcaadf7c2528f0764c4fdbef06d2
Wiebe Kaai <wiebekaai@gmail.com>
2023-09-10 04:04:11 +0200

    asd

--- src/elements/cart-drawer.js
+++ src/elements/cart-drawer.js
@@ -1,17 +1,18 @@
 export default class CartDrawer extends HTMLElement {
-  renderContents(a: any) {
+  renderContents(a) {
     const s = a.sections['main-product'];
     const html = new DOMParser().parseFromString(s, 'text/html');
 
     html.querySelectorAll('[data-line-item]').forEach((l) => {
-      const id = l.getAttribute('data-line-item')!;
+      const id = l.getAttribute('data-line-item');
 
-      const lineItem = this.querySelector(`[data-line-item="${id}"]`)!;
+      const lineItem = this.querySelector(`[data-line-item="${id}"]`);
+      const a = ''
 
       const replaceContents = ['[data-title]', '[data-quantity]'];
 
       replaceContents.forEach((c) => {
-        lineItem.querySelector(c)!.textContent = html.querySelector(c)?.textContent || '';
+        lineItem.querySelector(c).textContent = html.querySelector(c)?.textContent || '';
       });
     });
   }
--- src/elements/product-form.ts
+++ src/elements/product-form.ts
@@ -8,29 +8,30 @@ export default class ProductForm extends HTMLElement {
     this.cart = document.querySelector('[data-cart-drawer]')!;
   }
 
-  connectedCallback() {
+  async connectedCallback() {
     const form = this.querySelector('form[action$="/cart/add"]')!;
 
-    form.addEventListener('submit', async (e) => {
-      e.preventDefault();
+    if (form)
+      form.addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-      const formData = new FormData(e.currentTarget as HTMLFormElement);
-      formData.append('sections', 'main-product');
-      formData.append('sections_url', window.location.pathname);
+        const formData = new FormData(e.currentTarget as HTMLFormElement);
+        formData.append('sections', 'main-product');
+        formData.append('sections_url', window.location.pathname);
 
-      this.cart.setLoading();
+        this.cart.setLoading();
 
-      const cart = await fetch(
-        `${window.routes.cart_add_url}
+        const cart = await fetch(
+          `${window.routes.cart_add_url}
       `,
-        { method: 'POST', headers: { Accept: `application/javascript` }, body: formData },
-      )
-        .then((response) => response.json())
-        .then((data) => data);
+          { method: 'POST', headers: { Accept: `application/javascript` }, body: formData },
+        )
+          .then((response) => response.json())
+          .then((data) => data);
 
-      this.cart.renderContents(cart);
+        this.cart.renderContents(cart);
 
-      this.cart.unsetLoading();
-    });
+        this.cart.unsetLoading();
+      });
   }
 }
